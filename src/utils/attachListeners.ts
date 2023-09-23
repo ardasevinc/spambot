c77b04617a1468db97e45b20f0938dc9ba74cc57
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-09-23 19:33:43 +0300

    feat: improved error handling, bug fixes

--- src/utils/attachListeners.ts
+++ src/utils/attachListeners.ts
@@ -2,12 +2,18 @@ import { clientReady } from '@/listeners/clientReady';
 import { interactionCreate } from '@/listeners/interactionCreate';
 import { Bot } from '@/types/Bot';
 import { Events } from 'discord.js';
-export const attachListeners = (Bot: Bot) => {
-  Bot.on(Events.InteractionCreate, async (interaction) => {
-    await interactionCreate(Bot, interaction);
-  });
+export const attachListeners = async (Bot: Bot) => {
+  try {
+    Bot.on(Events.InteractionCreate, async (interaction) => {
+      await interactionCreate(Bot, interaction);
+    });
 
-  Bot.once(Events.ClientReady, async () => {
-    await clientReady(Bot);
-  });
+    Bot.once(Events.ClientReady, async () => {
+      await clientReady(Bot);
+    });
+    return true;
+  } catch (err) {
+    console.error(`Event handler error: ${err}`);
+    return false;
+  }
 };
