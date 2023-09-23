91285cbf67135a0d73490bd766e8bd6b309df84f
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-09-23 19:09:41 +0300

    feat: type for env of bot process

--- src/types/Bot.ts
+++ src/types/Bot.ts
@@ -1,8 +1,9 @@
 import type { PrismaClient } from '@prisma/client';
 import type { Client } from 'discord.js';
-import type {  CommandCollection } from './Command';
+import type { CommandCollection } from './Command';
 
 export interface Bot extends Client {
   commands: CommandCollection;
+  env: Record<string, string>;
   db: PrismaClient;
 }
